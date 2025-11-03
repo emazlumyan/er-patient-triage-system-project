be9029962612f935d1eb4e3e59dc6bad1b6721b5
Eric Mazlumyan <emazlumyan@gmail.com>
2025-11-02 16:12:48 -0800

    Add files via upload

---
+++ Patient.h
@@ -0,0 +1,100 @@
+// File: Patient.h
+// Author: Eric Mazlumyan
+// Date: October 2025
+
+#ifndef PATIENT_H
+#define PATIENT_H
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Patient {
+private:
+    // Declare variables
+    string name;
+    int age;
+    int waitTime;
+    int triageCategory;
+    string symptom;
+    int priority;
+
+public:
+    // Define default constructor
+    Patient() : name(""), age(0), waitTime(0), triageCategory(1), symptom(""), priority(0) {
+    }
+
+    // Define parameterized constructor
+    Patient(string n, int a, int wT, int tC, string s) : name(n), age(a), waitTime(wT), triageCategory(tC), symptom(s) {
+        calculatePriority();
+    }
+
+    void calculatePriority() { // Define method to calculate Priority based on triage category, wait time, and age
+        int triageValue = 0;
+        switch (triageCategory) { // Define triageValue based on triage category
+            case 5: 
+                triageValue = 60;
+                break;
+            case 4:
+                triageValue = 48;
+                break;
+            case 3:
+                triageValue = 36;
+                break;
+            case 2:
+                triageValue = 24;
+                break;
+            case 1:
+                triageValue = 12;
+                break;
+        }
+
+        int waitValue = 0;
+        if (waitTime >= 60) { // Define waitValue based on wait time
+            waitValue = 24;
+        }
+        else if (waitTime >= 30) {
+            waitValue = 16;
+        }
+        else if (waitTime >= 15) {
+            waitValue = 8;
+        }
+
+        int ageValue = 0;
+        if (age > 64 || age < 18) { // Define ageValue based on age
+            ageValue = 5;
+        }
+
+        priority = triageValue + waitValue + ageValue; // Compute priority
+    }
+
+    // Getter methods
+    string getName() const {
+        return name;
+    }
+    int getAge() const {
+        return age;
+    }
+    int getTriage() const {
+        return triageCategory;
+    }
+    string getSymptom() const {
+        return symptom;
+    }
+    int getPriority() const {
+        return priority;
+    }
+
+    void increasePriority() { // Define method to increase priority by 5 with cap of 100
+        priority += 5;
+        if (priority > 100) {
+            priority = 100;
+        }
+    }
+
+    void printInfo() const { // Define method to print patient information in one line
+        cout << name << ", Age " << age << ", Triage " << triageCategory << ": " << symptom;
+    }
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,103 @@
+// File: main.cpp
+// Author: Eric Mazlumyan
+// Date: October 2025
+
+#include <iostream>
+#include "MaxHeap.h"
+using namespace std;
+
+int main() {
+    // Declare variables
+    MaxHeap queue;
+    int choice;
+
+    while (true) { // Check that the user has not exited program
+        cout << "=== ER Patient Management Menu ===\n";
+        cout << "1. Add new patient\n";
+        cout << "2. Display current queue\n";
+        cout << "3. Process next patient\n";
+        cout << "4. View highest priority patient\n";
+        cout << "5. Update a patient's priority (condition worsening)\n";
+        cout << "6. Show number of patients waiting\n";
+        cout << "7. Exit\n";
+        cout << "Enter choice: ";
+        cin >> choice;
+
+        if (choice == 1) {
+            // Declare variables
+            string name;
+            int age;
+            int waitTime;
+            int triageCategory;
+            string symptom;
+
+            // Prompt the user and accept input from user
+            cout << "Enter patient first and last name: ";
+            cin.ignore();
+            getline(cin, name);
+            cout << "Enter patient age: ";
+            cin >> age;
+            cout << "Enter the number of minutes since the patient arrived: ";
+            cin >> waitTime;
+            cout << "Enter triage category (1-5): ";
+            cin >> triageCategory;
+            cin.ignore();
+            cout << "Enter symptom: ";
+            getline(cin, symptom);
+
+            // Add new patient
+            Patient newPatient(name, age, waitTime, triageCategory, symptom);
+            cout << "New patient arrived!\n";
+            cout << "Adding: ";
+            newPatient.printInfo();
+            cout << endl;
+            queue.Insert(newPatient); 
+        }
+        else if (choice == 2) {
+            queue.DisplayQueue();
+        }
+        else if (choice == 3) {
+            cout << "Processing next patient...\n";
+            Patient nextPatient = queue.ExtractMax();
+            cout << "TREATING: ";
+            nextPatient.printInfo();
+            cout << endl;
+        }
+        else if (choice == 4) {
+            cout << "Viewing highest priority patient...\n";
+            Patient priorityPatient = queue.Peek();
+            cout << "[Priority: " << priorityPatient.getPriority() << "] ";
+            priorityPatient.printInfo();
+            cout << endl;
+        }
+        else if (choice == 5) {
+            // Prompt the user and accept input from user for determining patient of interest
+            cout << "Select which patient's priority to increase:\n";
+            queue.DisplayQueue();
+            cout << "Enter patient number (1-" << queue.Size() << "): ";
+            int patientNumber;
+            cin >> patientNumber;
+
+            if (patientNumber < 1 || patientNumber > queue.Size()) { // Validate input
+                cout << "Invalid selection.\n";
+            }
+            else { // Increase the priority of patient of interest by 5 with cap of 100
+                queue.IncreaseKey(patientNumber - 1);
+                cout << "Patient's priority updated.\n";
+            }
+        }
+        else if (choice == 6) {
+            cout << "The number of patients currently waiting is: " << queue.Size() << ".\n";
+        }
+        else if (choice == 7) {
+            cout << "Exiting system.\n";
+            break;
+        }
+        else {
+            cout << "Invalid selection. Try again.\n";
+        }
+
+    }
+
+    return 0;
+}
